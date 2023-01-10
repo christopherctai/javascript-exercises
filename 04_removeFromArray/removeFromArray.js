75c50f833b191b051f33f8003d7d0292642da74b
Chris Tai <christopherctai@gmail.com>
2023-01-10 15:16:53 -0800

    Write function for removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    array = arguments[0];
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 1; j < (arguments.length); j++) {
+            if (array[i] === arguments[j]) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return arguments[0];
 };
 
 // Do not edit below this line
