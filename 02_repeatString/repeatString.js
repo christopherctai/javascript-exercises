80caa2672ca6de9ca641920478fd6e98edf63578
Chris Tai <christopherctai@gmail.com>
2023-01-10 14:34:00 -0800

    Complete repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+    output = '';
+    if (repeats < 0) {
+        return "ERROR"
+    }
+    for (let i = 0; i < repeats; i++) {
+            output += string;
+    };
+    return output;
 };
 
 // Do not edit below this line
