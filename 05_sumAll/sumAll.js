bd96a0a879294c228a1694782015f0206d21f57b
Chris Tai <christopherctai@gmail.com>
2023-01-10 15:51:14 -0800

    Complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(firstInteger, secondInteger) {
+    if (firstInteger < 0 || secondInteger < 0 || typeof(firstInteger) != "number" || typeof(secondInteger) != "number") return "ERROR";
+    let sum = 0;
+    if (firstInteger > secondInteger) {
+        [firstInteger, secondInteger] = [secondInteger, firstInteger];
+    }
+    for (firstInteger; firstInteger < secondInteger + 1; firstInteger++) {
+        sum += firstInteger;
+    }
+    return sum;
 };
 
 // Do not edit below this line
