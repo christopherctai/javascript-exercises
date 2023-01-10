264d77c4760c347aaa0713f4e301aac931049fde
Chris Tai <christopherctai@gmail.com>
2023-01-10 14:53:12 -0800

    Complete reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let reversedArray = [];
+    for (let i = 1; i <= stringArray.length; i++) {
+        reversedArray.push(stringArray[stringArray.length - i]);
+    }
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
