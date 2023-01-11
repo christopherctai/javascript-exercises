3e739c461bb7a51a0bdeedfcfdd28b919f6acf86
Chris Tai <christopherctai@gmail.com>
2023-01-10 16:19:39 -0800

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  temperature = Math.round(((temperature - 32) * (5/9)) * 10) / 10;
+  return temperature;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  temperature = Math.round(((9/5) * temperature + 32) * 10) / 10;
+  return temperature
 };
 
 // Do not edit below this line
